173000f9fb0e70f2d3d632eb2d4d13a825ff448f
franberfer <franberfer11@gmail.com>
2024-05-15 18:57:48 -0300

    login modified

--- views/componentes/header.php
+++ views/componentes/header.php
@@ -3,21 +3,24 @@
         <img src="/build/src/images/LOGO2.png" alt="Logo Iparraguirre Motors">
     </a>
 
+    <?php $url = $_SERVER["REQUEST_URI"]; ?>
+    <div class="<?php if ($url === '/auth/login') echo "oculto";?>">
+    
+        <?php include __DIR__ . "/navegacion.php"; ?>
 
-    <?php include __DIR__ . "/navegacion.php"; ?>
+        <div class="brgMenu brgMenuHandler">
+            <img src="/build/src/images/brgMenu.svg" alt="Hamburger Menu">
+        </div>
 
-    <div class="brgMenu brgMenuHandler">
-        <img src="/build/src/images/brgMenu.svg" alt="Hamburger Menu">
-    </div>
-
-    <div class="mobile-menu menu-disabled">
-        <section class="mobile-menu-header">
-            <div class="quit-menu-img brgMenuHandler">
-                <img src="/build/src/images/arrowLeft.svg" alt="quit menu left arrow">
-            </div>
-        </section>
-        <section class="mobile-menu-content">
-            <?php include __DIR__ . "/navegacion.php"; ?>
-        </section>
+        <div class="mobile-menu menu-disabled">
+            <section class="mobile-menu-header">
+                <div class="quit-menu-img brgMenuHandler">
+                    <img src="/build/src/images/arrowLeft.svg" alt="quit menu left arrow">
+                </div>
+            </section>
+            <section class="mobile-menu-content">
+                <?php include __DIR__ . "/navegacion.php"; ?>
+            </section>
+        </div>
     </div>
 </header>
