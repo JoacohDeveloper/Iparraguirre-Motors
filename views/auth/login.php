1ebbb24a2db3ee763790ba794c83ddc053b61e71
franberfer <franberfer11@gmail.com>
2024-05-24 17:49:06 -0300

    register & login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,30 +1,30 @@
 <div class="container" style="justify-content: center; align-items: center;">
 
 <section>
-        <h1>LOGIN</h1>
-        <form class="f_login" method="POST" action="">
-            <?php if (isset($errores)) {
-                    foreach ($errores as $key => $value) {
-                        echo "<div class='error'>$value</div>";
-                    }
-                } ?>
-            <div>
-                <p id="email_txt">Email</p>
-                <input class="inputs" type="text" name="email" placeholder="Usuario o Email">
-                <div id="primera_linea"></div>
-            </div>
-            <div>
-                <p id="password_txt">Password</p>
-                <input class="inputs" type="password" name="password" placeholder="Password">
-                <div id="segunda_linea"></div>
-            </div>
-            <div class="remember">
-                <input type="checkbox">
-                <p id="remember_txt">Remember me</p>
-            </div>
-            <input id="boton" type="submit" value="Ingresar">
-            <p id="regist_txt">Sign Up</p>
-        </form>
-    </section>
-    
+    <h1>LOGIN</h1>
+    <form method="POST" action="">
+        <?php if (isset($errores)) {
+                foreach ($errores as $key => $value) {
+                    echo "<div class='error'>$value</div>";
+                }
+            } ?>
+        <div>
+            <p class="inputs_txt">Email</p>
+            <input class="inputs" type="text" name="email" placeholder="Usuario o Email">
+            <div id="primera_linea"></div>
+        </div>
+        <div>
+            <p class="inputs_txt">Password</p>
+            <input class="inputs" type="password" name="password" placeholder="Password">
+            <div id="segunda_linea"></div>
+        </div>
+        <div class="remember">
+            <input type="checkbox">
+            <p id="remember_txt">Remember me</p>
+        </div>
+        <input id="boton" type="submit" value="Ingresar">
+        <p id="regist_txt">Sign Up</p>
+    </form>
+</section>
+
 </div>
--- views/auth/register.php
+++ views/auth/register.php
@@ -1,27 +1,45 @@
 <div class="container" style="justify-content: center; align-items: center; flex-direction: column;">
-    <h2>Hello world from register</h2>
-    <div class="errores">
-        <?php
-        if (isset($errores)) {
-            foreach ($errores as $key => $value) {
-                echo "<div class='error'>$value</div>";
-            }
-        } ?>
-    </div>
 
-    <form action="/auth/register" method="post" id="f_register">
-
-        <input type="text" name="fullname" placeholder="Nombre Completo" value="<?php echo $campos["fullname"] ?>">
-
-        <input type="text" name="username" placeholder="Username" value="<?php echo $campos["username"] ?>">
-
-        <input type="text" name="email" placeholder="Email" value="<?php echo $campos["email"] ?>">
-
-        <input type="password" name="password" placeholder="Password">
-
-        <input type="password" name="re_password" placeholder="Repetir Password">
-
-        <input type="submit" value="enviar">
-
+<section>
+    <h1>Register</h1>
+    <form method="POST" action="">
+        <?php if (isset($errores)) {
+                foreach ($errores as $key => $value) {
+                    echo "<div class='error'>$value</div>";
+                }
+            } ?>
+        <div>
+            <p class="inputs_txt" id="fullname_txt">Fullname</p>
+            <input class="inputs" type="text" name="fullname" placeholder="Nombre Completo" value="<?php echo $campos["fullname"] ?>">
+            <div id="primera_linea"></div>
+        </div>
+        <div>
+            <p class="inputs_txt" id="username_txt">Username</p>
+            <input class="inputs" type="text" name="username" placeholder="Username" value="<?php echo $campos["username"] ?>">
+            <div id="segunda_linea"></div>
+        </div>
+        <div>
+            <p class="inputs_txt">Email</p>
+            <input class="inputs" type="text" name="email" placeholder="Email" value="<?php echo $campos["email"] ?>">
+            <div id="tercera_linea"></div>
+        </div>
+        <div>
+            <p class="inputs_txt">Password</p>
+            <input class="inputs" type="password" name="password" placeholder="Password">
+            <div id="cuarta_linea"></div>
+        </div>
+        <div>
+            <p class="inputs_txt" id="repeat-password_txt">Repeat password</p>
+            <input class="inputs" type="password" name="re_password" placeholder="Repetir Password">
+            <div id="quinta_linea"></div>
+        </div>
+        <div class="terms">
+            <input type="checkbox">
+            <p id="terms_txt">Acepto los terminos y condiciones</p>
+        </div>
+        <input id="boton" type="submit" value="Registrar">
+        <p id="login_txt">Log In</p>
     </form>
+</section>
+
 </div>
