32a20c7afc55930b3d72f496cc8dc5ef5aeca06e
JoacohDeveloper <intelcorei5gamer@gmail.com>
2024-06-25 21:32:14 -0300

    fix merge

--- includes/helpers.php
+++ includes/helpers.php
@@ -15,11 +15,7 @@ function sanitize($value)
 }
 
 
-<<<<<<< HEAD
 function implementFile($url = "", $args = null)
-=======
-function implementFile($url = "")
->>>>>>> d_changes/add-settings-view
 {
     $root = str_replace("/public", "", str_replace("\\", "/", $_SERVER["DOCUMENT_ROOT"]));
 
@@ -27,27 +23,18 @@ function implementFile($url = "")
 
     return $includeFile;
 }
-<<<<<<< HEAD
 function implementComp($url = "", $params = [])
-{   
+{
 
     foreach ($params as $key => $value) {
         $$key = $value;
     }
     ob_start();
-=======
-function implementComp($url = "")
-{
->>>>>>> d_changes/add-settings-view
     $root = str_replace("/public", "/views/componentes/", str_replace("\\", "/", $_SERVER["DOCUMENT_ROOT"]));
 
     $includeFile = $root . $url;
 
-<<<<<<< HEAD
     include_once $includeFile;
 
     echo ob_get_clean();
-=======
-    return $includeFile;
->>>>>>> d_changes/add-settings-view
 }
