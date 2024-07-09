000223418c10cafa5be8a5d4347cd96f6ddb6835
SoyGlady <smtechnologycontacto@gmail.com>
2024-07-09 00:41:23 -0300

    Carrusel de logos index + modificacion en Marcas

--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@
     <div class="banner">
         <div class="banner-content">
             <h1>Iparraguirre <br> <span>Motors</span></h1>
-            <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque praesentium quam earum voluptate fugiat, vero placeat quos repudiandae.</p> -->
+         <p>Descubre el lujo en Iparraguirre Motors, tu agencia de autos exóticos.<br> Encuentra tu vehículo de ensueño entre nuestra selección exclusiva. <br> ¡Bienvenido a la excelencia automotriz!</p>
             <a href="#">Mostrar Catalogo</a>
         </div>
         <div class="downside">
@@ -44,6 +44,163 @@
                 </div>
             </div>
 
+        </div>
+
+        <div>
+            <div class="slider">
+            <div class="slider-track">
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Abarth.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Acura.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/AlfaRomeo.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Alpine.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Aston Martin.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Audi.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Betnley.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Bmw.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Byd.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Cadilac.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Chevrolet.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Chrysler.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Dodge.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Ferrari.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Fiat.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Ford.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Genesis.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Honda.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/International.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Abarth.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Acura.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/AlfaRomeo.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Alpine.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Aston Martin.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Audi.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Betnley.png" alt="">
+                </div>
+            
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Bmw.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Byd.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Cadilac.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Chevrolet.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Chrysler.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Dodge.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Ferrari.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Fiat.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Ford.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Genesis.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/Honda.png" alt="">
+                </div>
+
+                <div class="slide">
+                <img src="/build/src/images/Marcas/International.png" alt="">
+                </div>
         </div>
     </div>
 </div>
