2c4b73b069d66d6aeee3e3f4488ee7311675d055
LautyCruz <lautycruz5@gmail.com>
2024-07-17 21:32:58 -0300

    feat(footer) footer desktop

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,12 +1,46 @@
 
 <footer class="<?php echo isset($class) ? $class : "" ?>">
 
-    <?php include __DIR__ . "/navegacion.php";
-    ?>
     <?php include __DIR__ . "/themeSwitcher.php"; ?>
 
-    <div class="img-container">
-        <img src="/build/src/images/LOGO.png" alt="Iparraguirre Logo">
+        <div class="footer_menu">
+            <div class="lista">
+                <h3>About</h3>
+                <ul>
+                    <li>Services</li>
+                    <li>Portfolio</li>
+                    <li>Pricing</li>
+                    <li>Customers</li>
+                </ul>
+            </div>
+
+            <div class="lista">
+                <h3>Resources</h3>
+                <ul>
+                    <li>Home</li>
+                    <li>Tienda</li>
+                    <li>Portfolio</li>
+                    <li>Account</li>
+                </ul>
+            </div>
+                    
+            <div class="lista">
+                <h3>Contact</h3>
+                <ul>
+                    <li>091-000-000</li>
+                    <li>contact@IparraguirreMotors.com</li>
+                    <li>Services</li>
+                </ul>
+            </div>
+        </div>
+            <div class="iconos">
+                <i class='bx bxl-facebook'></i>
+                <i class='bx bxl-instagram'></i>
+                <i class='bx bxl-linkedin'></i>
+                <i class='bx bxl-twitter'></i>
+            </div>
+
+    <div class="copy">
+        <h3>2024 &copy; Copyright Iparraguirre Motores</h3>
     </div>
-    <p>Iparraguire Motors &copy; <?php echo date("Y"); ?></p>
 </footer>
