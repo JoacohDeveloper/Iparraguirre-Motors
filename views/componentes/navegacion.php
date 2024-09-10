9c7ac858c2af16ba5ee9a2a201fa99046ee3af57
franberfer <franberfer11@gmail.com>
2024-09-09 21:37:05 -0300

    ref(index) footer & account drop refactorized

--- views/componentes/navegacion.php
+++ views/componentes/navegacion.php
@@ -3,9 +3,9 @@
 
 <nav class="navegacion">
     <ul>
-        <li><a href="/" class="<?php echo $url === '/' ? 'actual-index' : '' ?>">Inicio</a></li>
-        <li><a href="/tienda" class="<?php echo (strpos($url, '/tienda') === 0) ? 'actual' : '' ?>">Tienda</a></li>
-        <li><a href="/faq" class="<?php echo $url === '/faq' ? 'actual' : '' ?>">Ayuda</a></li>
-        <li><a href="/auth" class="<?php echo $url === '/auth' ? 'actual' : '' ?>">Iniciar sesion</a></li>
+        <li><a href="/" class="nav-links <?php echo $url === '/' ? 'actual-index' : '' ?>">Inicio</a></li>
+        <li><a href="/tienda" class="nav-links <?php echo (strpos($url, '/tienda') === 0) ? 'actual' : '' ?>">Tienda</a></li>
+        <li><a href="/faq" class="nav-links <?php echo $url === '/faq' ? 'actual' : '' ?>">Ayuda</a></li>
+        <li><a href="/auth" class="nav-links <?php echo $url === '/auth' ? 'actual' : '' ?>">Iniciar sesion</a></li>
     </ul>
 </nav>
